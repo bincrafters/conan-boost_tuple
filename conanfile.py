a7f8f827c18a95a17681e2e547d0d413459cb028
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:35:00 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,16 +2,16 @@ from conans import ConanFile, tools, os
 
 class BoostTupleConan(ConanFile):
     name = "Boost.Tuple"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-tuple"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["tuple"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing"
  
     def source(self):
         boostorg_github = "https://github.com/boostorg"
