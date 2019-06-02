7dc3bb4b852e3d87d151bab80209f37376a1ac7c
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:09 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostTupleConan(base.BoostBaseConan):
     name = "boost_tuple"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_tuple"
-    lib_short_names = ["tuple"]
-    header_only_libs = ["tuple"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
